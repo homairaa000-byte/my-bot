cf709f9d0e469cdf5e0cb79ecdf5b9b3d2377279
homairaa000-byte <homairaa000@gmail.com>
2026-06-22 18:15:14 +0200

    Create database.py

---
+++ database.py
@@ -0,0 +1,17 @@
+import aiosqlite
+
+DB_NAME = "bot.db"
+
+async def init_db():
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            user_id INTEGER PRIMARY KEY
+        )
+        """)
+        await db.commit()
+
+async def add_user(user_id: int):
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute("INSERT OR IGNORE INTO users (user_id) VALUES (?)", (user_id,))
+        await db.commit()
