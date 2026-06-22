c5962b95be478073e8b2b7b031d88ea92d53a8b6
homairaa000-byte <homairaa000@gmail.com>
2026-06-22 17:15:18 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,8 @@
 import os
 import logging
-import threading
-from http.server import BaseHTTPRequestHandler, HTTPServer
-
+import asyncio
 from telegram import Update
-from telegram.ext import (
-    Application,
-    CommandHandler,
-    ContextTypes,
-)
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 from handlers.start import start
 
@@ -17,32 +11,20 @@ logging.basicConfig(level=logging.INFO)
 TOKEN = os.getenv("BOT_TOKEN")
 
 
-# ===== Web server بسيط لـ Render =====
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b"Bot is running")
-
-
-def run_server():
-    port = int(os.environ.get("PORT", 10000))
-    server = HTTPServer(("0.0.0.0", port), Handler)
-    server.serve_forever()
-
-
-# ===== Telegram bot =====
-def main():
+async def main():
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
 
-    # تشغيل السيرفر + البوت معًا
-    threading.Thread(target=run_server).start()
-
     print("Bot is running...")
-    app.run_polling()
+
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
+
+    # يخلي البوت شغال للأبد
+    await asyncio.Event().wait()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
